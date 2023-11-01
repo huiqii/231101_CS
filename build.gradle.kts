167cdafa5c4fc9bab3ba442d25c847000fb1884b
huiqii <zxcv729820@gmail.com>
2023-11-01 15:00:53 +0800

    Update build.gradle.kts
    Add 'application' plugin
    set encoding 'UTF-8'

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 plugins {
     id("java")
+    application
 }
 
 group = "com.github.huiqii"
 version = "1.0-SNAPSHOT"
+application {
+    mainClass = "com.github.jyhsu2000.Main"
 
+}
 repositories {
     mavenCentral()
 }
@@ -12,6 +16,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
 }
 
 tasks.test {
